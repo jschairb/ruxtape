62345310cf06fa8592a249786d1f2b1e34343dfb
Josh Schairbaum <joshua.schairbaum@gmail.com>
2008-08-27 13:29:17 -0400

    moving lib to vendor

--- ruxtape.rb
+++ ruxtape.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-$:.unshift File.dirname(__FILE__) + "/lib"
+$:.unshift File.dirname(__FILE__) + "/vendor"
 %w(camping mime/types mp3info).each { |lib| require lib}
 
 Camping.goes :Ruxtape
