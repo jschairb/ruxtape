328e08bad91134f1d68707570de4750ecbc0fd0d
Josh Schairbaum <joshua.schairbaum@gmail.com>
2008-08-30 07:16:32 -0400

    moved Cookie::Session to lib/, finished upload, working on OpenID

--- ruxtape.rb
+++ ruxtape.rb
@@ -1,79 +1,61 @@
 #!/usr/bin/env ruby
 
 $:.unshift File.dirname(__FILE__) + "/vendor"
-%w(camping mime/types mp3info yaml openid base64).each { |lib| require lib}
-
-module Camping
-  module CookieSessions
-    def service(*a)
-      if @cookies.identity
-        blob, secure_hash = @cookies.identity.to_s.split(':', 2)
-        blob = Base64.decode64(blob)
-        data = Marshal.restore(blob)
-        data = {} unless secure_blob_hasher(blob).strip.downcase == secure_hash.strip.downcase
-      else
-        blob = ''; data = {}
-      end
-      
-      app = self.class.name.gsub(/^(\w+)::.+$/, '\1')
-      @state = (data[app] ||= Camping::H[])
-      hash_before = blob.hash
-      return super(*a)
-    ensure
-      data[app] = @state
-      blob = Marshal.dump(data)
-      unless hash_before == blob.hash
-        secure_hash = secure_blob_hasher(blob)
-        @cookies.identity = Base64.encode64(blob).gsub("\n", '').strip + ':' + secure_hash
-        @headers['Set-Cookie'] = @cookies.map { |k,v| "#{k}=#{C.escape(v)}; path=#{self/"/"}" if v != @k[k] } - [nil]
-      end
-    end
-    
-    def secure_blob_hasher(data)
-      require 'digest'
-      require 'digest/sha2'
-      Digest::SHA512::hexdigest(self.class.module_eval('@@state_secret') + data)
-    end
-  end
-end
+$:.unshift File.dirname(__FILE__) + "/lib"
+%w(camping camping_addons fileutils mime/types mp3info yaml openid base64).each { |lib| require lib}
 
 Camping.goes :Ruxtape
 
 module Ruxtape
   include Camping::CookieSessions
+  MP3_PATH = File.join(File.expand_path(File.dirname(__FILE__)), 'public', 'songs')
   @@state_secret = "27c9436319ae7c1e760dbd344de08f82b4c7cfcf"
 end
 
 module Ruxtape::Models
   class Config
     CONFIG_FILE = File.join(File.expand_path(File.dirname(__FILE__)), 'config', 'config.yml')
-    attr_accessor :values
+    cattr_accessor
     class << self
+      def delete; File.delete(CONFIG_FILE); end
       def setup?; return true if File.exist?(CONFIG_FILE) end
-      def load; values = YAML.load_file(CONFIG_FILE); end
+      def values; YAML.load_file(CONFIG_FILE); end
       def setup(openid)  
         File.open(CONFIG_FILE, "w") { |f| YAML.dump(openid, f) }
       end
     end
   end
 
+  class Destroyer
+    def self.run
+      Config.delete; Song.ruxtape_delete
+    end
+  end
+
+  class Mixtape
+    class << self
+    def delete
+      Dir.glob("#{Ruxtape::MP3_PATH}/*.mp3").each { |mp3| File.delete(mp3) }
+    end
+    def playlist
+      songs = []
+      Dir.glob("#{Ruxtape::MP3_PATH}/*.mp3").each { |mp3| songs << Song.new(mp3) }
+      return songs
+    end
+    def song_count; Dir.glob("#{Ruxtape::MP3_PATH}/*.mp3").length; end
+    def length; "17 min 22 secs"; end  
+    end
+  end
+
   class Song
-    MP3_PATH = File.join(File.expand_path(File.dirname(__FILE__)), 'public', 'songs')
-    attr_accessor :title, :artist, :length
+    attr_accessor :title, :artist, :length, :filename
+    attr_reader :path
     def initialize(path) 
-      @path = path 
-      Mp3Info.open(@path) do |mp3|
+      @path = path
+      Mp3Info.open(path) do |mp3|
         self.title, self.artist, self.length = mp3.tag.title, mp3.tag.artist, mp3.length
       end
     end
-    def self.ruxtape
-      songs = []
-      Dir.glob("#{MP3_PATH}/*.mp3").each { |mp3| songs << Song.new(mp3) }
-      return songs
-    end
-
-    def self.ruxtape_song_count; Dir.glob("#{MP3_PATH}/*.mp3").length; end
-    def self.ruxtape_time; "17 min 22 secs"; end
 
     def time
       minutes = (length/60).to_i; seconds = (((length/60) - minutes) * 60).to_i
@@ -87,7 +69,7 @@ module Ruxtape::Controllers
   class Index < R '/'
     def get
       if Config.setup? 
-        @songs = Song.ruxtape 
+        @songs = Mixtape.playlist
         render(:index) 
       else 
         render(:setup)
@@ -97,12 +79,15 @@ module Ruxtape::Controllers
 
   class Admin < R '/admin'
     def get
+      return redirect('/setup') unless @state.identity
+      @songs = Mixtape.playlist
       render :admin
     end
   end
 
   class Login < R '/login'
     def get
+      return redirect R(Index) unless Config.values[:openid] == input.openid_identifier
       this_url = 'http' + URL('/login').to_s
       unless input.finish.to_s == '1'
         begin
@@ -121,7 +106,7 @@ module Ruxtape::Controllers
         case response.status
         when OpenID::Consumer::SUCCESS
           @state.identity = response.identity_url.to_s
-          return redirect(R(HomeScreen))
+          return redirect(R(Admin))
         when OpenID::Consumer::FAILURE
           'The OpenID thing doesn\'t think you really are that person, they said: ' + response.message
         end
@@ -129,9 +114,16 @@ module Ruxtape::Controllers
     end
   end
 
+  class Restart < R '/restart'
+    def post
+      return redirect('/setup') unless @state.identity
+      Destroyer.run
+      redirect R(Index)
+    end
+  end
+
   class Setup < R '/setup'
     def get; Config.setup? ? render(:setup) : redirect(R(Index)); end
-
     def post
       unless Config.setup?
         Config.setup(:openid => input.openid_address)
@@ -143,9 +135,18 @@ module Ruxtape::Controllers
   end
 
   class Upload < R '/upload'
+    include FileUtils::Verbose
     def post
-      file = input.file
-
+      return redirect('/setup') unless @state.identity
+      @file_attrs = { :filename     => input.file[:filename],
+                      :content_type => input.file[:type],
+                      :content      => input.file[:tempfile] }
+      @path = File.join(Song::Ruxtape::MP3_PATH, @file_attrs[:filename])
+      # This works as well, but reads into memory a second time.
+#       File.open(@path, 'w') do |file|
+#         file << @file_attrs[:content].read
+#       end
+      cp(@file_attrs[:content].path, @path)
       redirect R(Admin)
     end
   end
@@ -172,15 +173,20 @@ module Ruxtape::Views
   def layout
     xhtml_strict do 
       head do 
-        title "Ruxtape"
+        title "Ruxtape => Punks jump up to get beat down."
         link(:rel => 'stylesheet', :type => 'text/css',
              :href => '/assets/styles.css', :media => 'screen' )
+        meta(:content => 'noindex, nofollow', :name => "robots")
+        script(:type => 'text/javascript', :src => '/assets/jquery.js')
+        script(:type => 'text/javascript', :src => '/assets/ruxtape.js')
+        unless @songs.nil?
+        end
       end
       body do 
         div.wrapper! do 
           div.header! do 
             div.title! { "Ruxtape, sucka"} 
-            div.subtitle! {"#{Ruxtape::Models::Song.ruxtape_song_count} songs, #{Ruxtape::Models::Song.ruxtape_time}"}
+            div.subtitle! {"#{Ruxtape::Models::Mixtape.song_count} songs, #{Ruxtape::Models::Mixtape.length}"}
           end
           self << yield
           div.footer! do 
@@ -227,6 +233,7 @@ module Ruxtape::Views
   def admin
     div.content! do 
       h1 "Switch Up Your Tape"
+      p 'You can upload another song, rearrange your mix, or blow it all away.'
       hr
       h2 "Upload a New Song"
       form({ :method => 'post', :enctype => "multipart/form-data", :action => R(Upload)}) do 
@@ -236,6 +243,17 @@ module Ruxtape::Views
       hr
       h2 "Rearrange Your Mixtape Order"
       p "Drag and drop to get the optimal soundz. (Saves automatically.)"
+      ul.sorter do 
+        @songs.each do |song|
+          li.sortable song.title
+        end
+      end
+      hr
+      h2 "Restart"
+      p "This will delete all your songs."
+      form({ :method => 'post', :action => R(Restart)}) do 
+        input :type => "submit", :value => "Restart"
+      end
     end
   end
 end
