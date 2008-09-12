c677da0e366d39a2fae698a317cce3751b38e5a6
Josh Schairbaum <joshua.schairbaum@gmail.com>
2008-09-12 09:15:43 -0400

    need to get our own version of mosquito on git and work there

--- test/test_ruxtape.rb
+++ test/test_ruxtape.rb
@@ -1,10 +1,14 @@
 require 'rubygems'
+$:.unshift File.dirname(__FILE__) + "/../vendor/mosquito/lib"
 require 'mosquito'
 require File.dirname(__FILE__) + "/../ruxtape"
 
 include Ruxtape::Models
 
 class TestConfig < Camping::ModelTest
+  test "should return true" do 
+    assert true
+  end
 end
 
 class TestMixtape < Camping::ModelTest
@@ -13,12 +17,12 @@ end
 class TestSong < Camping::ModelTest
 end
 
-class TestRuxtape < Camping::WebTest
+# class TestRuxtape < Camping::WebTest
 
-  def standard_gets
-    get "/"
-    assert_response :success
-    assert_match_body %r!Ruxtape!
-  end
+#   test "should get index" do 
+#     get "/"
+#     assert_response :success
+#     assert_match_body %r!Ruxtape!
+#   end
 
-end
+# end
